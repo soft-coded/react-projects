eba059b134a23e46c78801212e2b015cbc33a2c8
soft-coded <simplemanbj@gmail.com>
2021-02-14 14:03:36 +0530

    Project 3

---
+++ project3/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import Review from "./review"
+
+function App(){
+  return <main>
+    <h1>Our Reviews</h1>
+    <hr/>
+    <Review/>
+  </main>
+}
+
+export default App
